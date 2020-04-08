b36892c98ec1f7548920d2a9b77458481e88195d
Renato Cavalcanti <renato@cavalcanti.be>
2020-04-08 14:34:40 +0200

    move SlickProjectionImpl to internal package

--- akka-projection-slick/src/main/scala/akka/projection/slick/SlickProjection.scala
+++ akka-projection-slick/src/main/scala/akka/projection/slick/SlickProjection.scala
@@ -5,17 +5,14 @@
 package akka.projection.slick
 
 import akka.Done
-import akka.actor.ClassicActorSystemProvider
-import akka.annotation.{ ApiMayChange, InternalApi }
+import akka.annotation.ApiMayChange
 import akka.projection.Projection
-import akka.stream.scaladsl.{ Keep, Sink, Source }
-import akka.stream.{ KillSwitch, KillSwitches }
-import org.slf4j.LoggerFactory
+import akka.projection.slick.internal.SlickProjectionImpl
+import akka.stream.scaladsl.Source
 import slick.basic.DatabaseConfig
 import slick.dbio.DBIO
 import slick.jdbc.JdbcProfile
 
-import scala.concurrent.Future
 import scala.reflect.ClassTag
 
 object SlickProjection {
@@ -26,54 +23,4 @@ object SlickProjection {
       offsetExtractor: StreamElement => Offset,
       databaseConfig: DatabaseConfig[P])(eventHandler: StreamElement => DBIO[Done]): Projection =
     new SlickProjectionImpl(projectionId, sourceProvider, offsetExtractor, databaseConfig, eventHandler)
-}
-
-@InternalApi
-private[projection] class SlickProjectionImpl[Offset, StreamElement, P <: JdbcProfile](
-    projectionId: String,
-    sourceProvider: Option[Offset] => Source[StreamElement, _],
-    offsetExtractor: StreamElement => Offset,
-    databaseConfig: DatabaseConfig[P],
-    eventHandler: StreamElement => DBIO[Done])
-    extends Projection {
-
-  private val logger = LoggerFactory.getLogger(this.getClass)
-
-  private val offsetStore = new OffsetStore(databaseConfig.db, databaseConfig.profile)
-
-  private var shutdown: Option[KillSwitch] = None
-
-  override def start()(implicit systemProvider: ClassicActorSystemProvider): Unit = {
-
-    implicit val dispatcher = systemProvider.classicSystem.dispatcher
-    import databaseConfig.profile.api._
-
-    val lastKnownOffset: Future[Option[Offset]] = offsetStore.readOffset(projectionId)
-
-    val killSwitch =
-      Source
-        .future(lastKnownOffset.map { offsetOpt =>
-          logger.debug(s"Starting projection '$projectionId' from offset '$offsetOpt' ")
-          sourceProvider(offsetOpt)
-        })
-        .flatMapConcat[StreamElement, Any](identity)
-        .mapAsync(1) { elt =>
-          // run user function and offset storage on the same transaction
-          // offset storage comes last, any side-effect in user function is therefore at-least-once
-          val txDBIO =
-            eventHandler(elt).flatMap(_ => offsetStore.saveOffset(projectionId, offsetExtractor(elt))).transactionally
-
-          databaseConfig.db.run(txDBIO)
-        }
-        .viaMat(KillSwitches.single)(Keep.right)
-        .toMat(Sink.ignore)(Keep.left)
-        .run()
-
-    shutdown = Some(killSwitch)
-  }
-
-  override def stop(): Future[Done] = {
-    shutdown.foreach(_.shutdown())
-    Future.successful(Done)
-  }
 }
---
+++ akka-projection-slick/src/main/scala/akka/projection/slick/internal/SlickProjectionImpl.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.projection.slick.internal
+
+import akka.Done
+import akka.actor.ClassicActorSystemProvider
+import akka.annotation.InternalApi
+import akka.projection.Projection
+import akka.projection.slick.OffsetStore
+import akka.stream.{ KillSwitch, KillSwitches }
+import akka.stream.scaladsl.{ Keep, Sink, Source }
+import org.slf4j.LoggerFactory
+import slick.basic.DatabaseConfig
+import slick.dbio.DBIO
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.Future
+@InternalApi
+private[projection] class SlickProjectionImpl[Offset, StreamElement, P <: JdbcProfile](
+    projectionId: String,
+    sourceProvider: Option[Offset] => Source[StreamElement, _],
+    offsetExtractor: StreamElement => Offset,
+    databaseConfig: DatabaseConfig[P],
+    eventHandler: StreamElement => DBIO[Done])
+    extends Projection {
+
+  private val logger = LoggerFactory.getLogger(this.getClass)
+
+  private val offsetStore = new OffsetStore(databaseConfig.db, databaseConfig.profile)
+
+  private var shutdown: Option[KillSwitch] = None
+
+  override def start()(implicit systemProvider: ClassicActorSystemProvider): Unit = {
+
+    implicit val dispatcher = systemProvider.classicSystem.dispatcher
+    import databaseConfig.profile.api._
+
+    val lastKnownOffset: Future[Option[Offset]] = offsetStore.readOffset(projectionId)
+
+    val killSwitch =
+      Source
+        .future(lastKnownOffset.map { offsetOpt =>
+          logger.debug(s"Starting projection '$projectionId' from offset '$offsetOpt' ")
+          sourceProvider(offsetOpt)
+        })
+        .flatMapConcat[StreamElement, Any](identity)
+        .mapAsync(1) { elt =>
+          // run user function and offset storage on the same transaction
+          // offset storage comes last, any side-effect in user function is therefore at-least-once
+          val txDBIO =
+            eventHandler(elt).flatMap(_ => offsetStore.saveOffset(projectionId, offsetExtractor(elt))).transactionally
+
+          databaseConfig.db.run(txDBIO)
+        }
+        .viaMat(KillSwitches.single)(Keep.right)
+        .toMat(Sink.ignore)(Keep.left)
+        .run()
+
+    shutdown = Some(killSwitch)
+  }
+
+  override def stop(): Future[Done] = {
+    shutdown.foreach(_.shutdown())
+    Future.successful(Done)
+  }
+}
